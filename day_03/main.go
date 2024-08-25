4014a703c89c34d1b5982518120aa05f3387a1e4
Chetan <jellybeans33124@gmail.com>
2024-08-25 22:50:26 +0530

    day3 part2

--- day_03/main.go
+++ day_03/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -16,129 +15,106 @@ func main() {
 	}
 	lines := strings.Split(string(data), "\n")
 	result := 0
-	added := make(map[string]bool)
 	for i := 0; i < len(lines); i++ {
 		for j := 0; j < len(lines[i]); j++ {
 			c := rune(lines[i][j])
-			if c != '.' && !unicode.IsDigit(c) {
+			if c == '*' {
+				var numbers []int
 				if j > 0 && unicode.IsDigit(rune(lines[i][j-1])) {
 					n := findNum(lines[i], j, 0)
-					_, exists := added[fmt.Sprintf("%d %d", i, n)]
-					if !exists {
 
-						num, err := strconv.Atoi(lines[i][n:j])
-						if err != nil {
-							log.Fatal("Error while converting string: ", num)
-						}
-						result += num
-						added[fmt.Sprintf("%d %d", i, n)] = true
+					num, err := strconv.Atoi(lines[i][n:j])
+					if err != nil {
+						log.Fatal("Error while converting string: ", num)
 					}
+					numbers = append(numbers, num)
 				}
+
 				if j < len(lines[i])-1 && unicode.IsDigit(rune(lines[i][j+1])) {
-					_, exists := added[fmt.Sprintf("%d %d", i, j+1)]
-					if !exists {
-						n := findNum(lines[i], j, 1)
-						num, err := strconv.Atoi(lines[i][j+1 : n])
-						if err != nil {
-							log.Fatal("Error while converting string: ", num)
-						}
-						result += num
-						added[fmt.Sprintf("%d %d", i, n)] = true
-
+					n := findNum(lines[i], j, 1)
+					num, err := strconv.Atoi(lines[i][j+1 : n])
+					if err != nil {
+						log.Fatal("Error while converting string: ", num)
 					}
+					numbers = append(numbers, num)
+
 				}
+
 				if i > 0 && unicode.IsDigit(rune(lines[i-1][j])) {
 					s := findNum(lines[i-1], j, 0)
-					_, exists := added[fmt.Sprintf("%d %d", i-1, s)]
-					if !exists {
-						e := findNum(lines[i-1], j, 1)
-						num, err := strconv.Atoi(lines[i-1][s:e])
-						if err != nil {
-							log.Fatal("error while converting string")
-						}
-						result += num
-						added[fmt.Sprintf("%d %d", i-1, s)] = true
+					e := findNum(lines[i-1], j, 1)
+					num, err := strconv.Atoi(lines[i-1][s:e])
+					if err != nil {
+						log.Fatal("error while converting string")
 					}
+					numbers = append(numbers, num)
 				} else {
 					if i > 0 && j > 0 && unicode.IsDigit(rune(lines[i-1][j-1])) {
 						n := findNum(lines[i-1], j, 0)
-						_, exists := added[fmt.Sprintf("%d %d", i-1, n)]
-						if !exists {
 
-							num, err := strconv.Atoi(lines[i-1][n:j])
-							if err != nil {
-								log.Fatal("error while converting string: ", err)
-
-							}
-							result += num
-							added[fmt.Sprintf("%d %d", i-1, n)] = true
-						}
-					}
-					if i > 0 && j < len(lines[i])-1 && unicode.IsDigit(rune(lines[i-1][j+1])) {
-						_, exists := added[fmt.Sprintf("%d %d", i-1, j+1)]
-						if !exists {
-
-							n := findNum(lines[i-1], j, 1)
-							num, err := strconv.Atoi(lines[i-1][j+1 : n])
-							if err != nil {
-								log.Fatal("error while converting string: ", err)
-							}
-							result += num
-							added[fmt.Sprintf("%d %d", i-1, j+1)] = true
+						num, err := strconv.Atoi(lines[i-1][n:j])
+						if err != nil {
+							log.Fatal("error while converting string: ", err)
 
 						}
+						numbers = append(numbers, num)
 					}
+				
+				if i > 0 && j < len(lines[i])-1 && unicode.IsDigit(rune(lines[i-1][j+1])) {
+
+					n := findNum(lines[i-1], j, 1)
+					num, err := strconv.Atoi(lines[i-1][j+1 : n])
+					if err != nil {
+						log.Fatal("error while converting string: ", err)
+					}
+					numbers = append(numbers, num)
+
 				}
+            }
+
 				if i < len(lines)-1 && unicode.IsDigit(rune(lines[i+1][j])) {
 					s := findNum(lines[i+1], j, 0)
-					_, exists := added[fmt.Sprintf("%d %d", i+1, s)]
-					if !exists {
-						e := findNum(lines[i+1], j, 1)
-						num, err := strconv.Atoi(lines[i+1][s:e])
-						if err != nil {
-							log.Fatal("error while converting string")
-						}
-						result += num
-						added[fmt.Sprintf("%d %d", i+1, s)] = true
+					e := findNum(lines[i+1], j, 1)
+					num, err := strconv.Atoi(lines[i+1][s:e])
+					if err != nil {
+						log.Fatal("error while converting string")
 					}
+					numbers = append(numbers, num)
 				} else {
 					if i < len(lines)-1 && j > 0 && unicode.IsDigit(rune(lines[i+1][j-1])) {
 						n := findNum(lines[i+1], j, 0)
-						_, exists := added[fmt.Sprintf("%d %d", i+1, n)]
-						if !exists {
 
-							num, err := strconv.Atoi(lines[i+1][n:j])
-							if err != nil {
-								log.Fatal("error while converting string: ", err)
-
-							}
-							result += num
-							added[fmt.Sprintf("%d %d", i+1, n)] = true
-						}
-					}
-					if i < len(lines)-1 && j < len(lines[i]) && unicode.IsDigit(rune(lines[i+1][j+1])) {
-						_, exists := added[fmt.Sprintf("%d %d", i+1, j+1)]
-						if !exists {
-
-							n := findNum(lines[i+1], j, 1)
-							num, err := strconv.Atoi(lines[i+1][j+1 : n])
-							if err != nil {
-                                fmt.Println(lines[i-1])
-                                fmt.Println(lines[i])
-								log.Fatal("error while converting string: ", err)
-							}
-							result += num
-							added[fmt.Sprintf("%d %d", i+1, j+1)] = true
+						num, err := strconv.Atoi(lines[i+1][n:j])
+						if err != nil {
+							log.Fatal("error while converting string: ", err)
 
 						}
+						numbers = append(numbers, num)
 					}
+				
+				if i < len(lines)-1 && j < len(lines[i]) && unicode.IsDigit(rune(lines[i+1][j+1])) {
+
+					n := findNum(lines[i+1], j, 1)
+					num, err := strconv.Atoi(lines[i+1][j+1 : n])
+					if err != nil {
+						log.Fatal("error while converting string: ", err)
+					}
+					numbers = append(numbers, num)
+
 				}
-
+            }
+              
+              if len(numbers) == 2 {
+                result = result + (numbers[0] * numbers[1])
+              }
 			}
+
 		}
+
 	}
-	println("result: ", result)
+    println("result: ",result)
 }
+
 func findNum(line string, current int, x int) int {
 
 	if x == 0 {
