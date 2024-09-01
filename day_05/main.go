003c0cb5dd32056177e1c6bb11e954be64b66c00
Chetan <jellybeans33124@gmail.com>
2024-09-01 18:55:50 +0530

    day5 part2

--- day_05/main.go
+++ day_05/main.go
@@ -1,72 +1,111 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-    data, err := os.ReadFile("input.txt")
-    if err != nil {
-        log.Fatal("error while reading data from file", err)
-    }
-    input := string(data)
-    maps := strings.SplitN(input, "\n\n", 2)
-    seedStr := strings.Split(maps[0], ":")
-    seeds := strings.Split(strings.Trim(seedStr[1], " "), " ")
-    maps = strings.Split(maps[1], "\n\n")
-    prevs := make([]int, len(seeds))
-    for i, seed := range seeds {
-        prevs[i], err = strconv.Atoi(strings.Trim(seed, " "))
-        if err != nil {
-            log.Fatal("prevsErr:", err)
-        }
-        mapLoop:
-        for _, m := range maps {
-            mapStr := strings.Split(m, ":")[1]
-            lines := strings.Split(mapStr, "\n")
-            for _, line := range lines {
-                if len(line) <= 0 {
-                    continue
-                }
-                values := strings.Split(line, " ")
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal("error while reading data from file", err)
+	}
+	input := string(data)
+	maps := strings.SplitN(input, "\n\n", 2)
+	seedStr := strings.Split(maps[0], ":")
+	seeds := strings.Split(strings.TrimSpace(seedStr[1]), " ")
+	maps = strings.Split(maps[1], "\n\n")
+	var rangeArr []string
+	result := math.MaxInt
+	for i, seed := range seeds {
+		if i%2 != 0 {
+			continue
+		}
+		rangeArr = append(rangeArr, fmt.Sprintf("%s %s 0", seed, seeds[i+1]))
+	}
 
+	for len(rangeArr) > 0 {
+		currRange := rangeArr[0]
+		rangeArr = rangeArr[1:]
+		seedParts := strings.Split(currRange, " ")
+		seedRangeSt, err := strconv.Atoi(seedParts[0])
+		found := false
 
-                srcStart, err := strconv.Atoi(strings.Trim(values[1], " "))
-                if err != nil {
-                    log.Fatal(err)
-                }
-                dstStart, err := strconv.Atoi(strings.Trim(values[0], " "))
-                if err != nil {
-                    log.Fatal(err)
-                }
-                if prevs[i] >= srcStart {
-                    valueRange, err := strconv.Atoi(values[2])
-                    if err != nil {
-                        log.Fatal(err)
-                    }   
-                    diff :=  prevs[i]- srcStart
-                    if diff < valueRange {
-                        if i == 1 {
-                        }
-                        prevs[i] = dstStart+diff
-                        continue mapLoop
-                    }  
+		if err != nil {
+			log.Fatal("error while reading data from file", err)
+		}
+		rangeLen, err := strconv.Atoi(seedParts[1])
 
+		if err != nil {
+			log.Fatal("error while reading data from file", err)
+		}
+		mapInd, err := strconv.Atoi(seedParts[2])
+		lastIdx := mapInd == len(maps)-1
+		if err != nil {
+			log.Fatal("error while reading data from file", err)
+		}
+
+		mapStr := strings.TrimSpace(strings.Split(maps[mapInd], ":")[1])
+		lines := strings.Split(strings.TrimSpace(mapStr), "\n")
+		for _, line := range lines {
+			values := strings.Split(line, " ")
+
+			srcStart, err := strconv.Atoi(strings.TrimSpace(values[1]))
+			if err != nil {
+				log.Fatal(err)
+			}
+			dstStart, err := strconv.Atoi(strings.TrimSpace(values[0]))
+			if err != nil {
+				log.Fatal(err)
+			}
+			if seedRangeSt >= srcStart {
+				valueRange, err := strconv.Atoi(values[2])
+				if err != nil {
+					log.Fatal(err)
+				}
+				diff := seedRangeSt - srcStart
+				if diff < valueRange {
+					found = true
+					if seedRangeSt+rangeLen <= srcStart+valueRange {
+						if lastIdx {
+							if result > dstStart+diff {
+								result = dstStart + diff
+							}
+						} else {
+							rangeArr = append(rangeArr, fmt.Sprintf("%d %d %d", dstStart+diff, rangeLen, mapInd+1))
+						}
+					} else {
+						firstLen := (srcStart+valueRange) - seedRangeSt 
+						if lastIdx {
+							if result > dstStart+diff {
+								result = dstStart + diff
+							}
+						} else {
+							rangeArr = append(rangeArr, fmt.Sprintf("%d %d %d", dstStart+diff, firstLen, mapInd+1))
+						}
+						rangeArr = append(rangeArr, fmt.Sprintf("%d %d %d", srcStart+valueRange, rangeLen-firstLen, mapInd))
+
+					}
+					break
 				}
 
 			}
 
-
 		}
+		if !found {
+			if lastIdx {
+				if result > seedRangeSt {
+					result = seedRangeSt
+				}
+			} else {
+				rangeArr = append(rangeArr, fmt.Sprintf("%d %d %d", seedRangeSt, rangeLen, mapInd+1))
+			}
+		}
+
 	}
-    result := prevs[0]
-    for _, prev := range prevs{
-        if prev < result{
-            result = prev
-        }
-    }
-    println("result: ", result)
+	println("result: ", result)
 }
