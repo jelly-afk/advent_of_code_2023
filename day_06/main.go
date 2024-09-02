7dacfde85643c9c0a268d02368fe73ec0480dfb9
Chetan <jellybeans33124@gmail.com>
2024-09-03 00:05:05 +0530

    day6 part2

--- day_06/main.go
+++ day_06/main.go
@@ -14,44 +14,44 @@ func main(){
     }
     input := string(data)
     parts := strings.Split(input, "\n")
-    timeStr := strings.TrimSpace(strings.Split(parts[0], ":")[1])
-    distStr := strings.TrimSpace(strings.Split(parts[1], ":")[1])
-    timeL := strings.Split(timeStr, " ")
-    distL := strings.Split(distStr, " ")
+    timeS := strings.Split(parts[0], ":")[1]
+    distS := strings.Split(parts[1], ":")[1]
+    timeS = strings.ReplaceAll(timeS, " ", "")
+    distS = strings.ReplaceAll(distS, " ", "")
+  
     result := 1
-    for i, timeS := range timeL {
-        time, err := strconv.Atoi(timeS)
-        if err != nil {
-            log.Fatal(err)
-        }
-        dist,err := strconv.Atoi(distL[i])
-        if err != nil {
-            log.Fatal(err)
-        }
-        left := 0
-        right := time
-        res := 1
-        for right > left {
-            mid := (right+left)/2
-            prod := mid*(time-mid)
-            if prod > dist {
-                if (mid-1)*(time-mid+1) < dist {
-                    res = mid
-                    break
-                }
-
-                right = mid
-            } else if prod < dist {
-                left = mid+1
-            } else {
+    time, err := strconv.Atoi(timeS)
+    if err != nil {
+        log.Fatal(err)
+    }
+    dist,err := strconv.Atoi(distS)
+    if err != nil {
+        log.Fatal(err)
+    }
+    left := 0
+    right := time
+    res := 1
+    for right > left {
+        mid := (right+left)/2
+        prod := mid*(time-mid)
+        if prod > dist {
+            if (mid-1)*(time-mid+1) < dist {
                 res = mid
                 break
             }
-            
+
+            right = mid
+        } else if prod < dist {
+            left = mid+1
+        } else {
+            res = mid
+            break
+        }
+
         }
         result = result * ((time-res*2)+1)
         
-    }
+    
     println("result: ", result)
 
 
