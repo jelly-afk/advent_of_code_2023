6afd60da8ed803b4a874cdb365242b2a81946011
Chetan <jellybeans33124@gmail.com>
2024-09-12 23:54:26 +0530

    day11 part2

--- day_11/main.go
+++ day_11/main.go
@@ -41,19 +41,9 @@ func main (){
     for i, curr := range galaxies {
         for j := i+1;j < len(galaxies);j++ {
             next := galaxies[j]
-            res := 0
-            if curr[0] == next[0]{
-                x := col[next[1]] - col[curr[1]]
-                res = next[1] - curr[1] + x
-            } else if curr[1] == next[1] {
-                x := row[next[0]] - row[curr[0]]
-                res = next[0] - curr[0] + x
-            } else {
-
-                x := diff(col[next[1]] , col[curr[1]])
-                y := diff(row[next[0]] , row[curr[0]])
-                res = diff(next[0] , curr[0]) + diff(next[1] , curr[1]) + x + y
-            }
+            x := diff(col[next[1]] , col[curr[1]])
+            y := diff(row[next[0]] , row[curr[0]]) 
+            res := diff(next[0] , curr[0]) + diff(next[1] , curr[1]) - (x + y) + ((x+y)*1000000)
 
             result+=res
         }
